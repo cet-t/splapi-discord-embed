e8a25850de9735a57becc20a2a9637bc26be255e
cet <cets021@gmail.com>
2026-09-02 20:13:32 +0900

    client を status に

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,11 @@ mod cliargs;
 mod helper;
 mod splatoon;
 
-use axum::{Router, routing::get};
+use axum::{
+    Router,
+    extract::{Query, State},
+    routing::get,
+};
 use clap::Parser;
 
 use crate::{
@@ -19,15 +23,12 @@ struct EmbedQuery {
     /// schedule index
     #[serde(alias = "i")]
     n: Option<u8>,
-    /// unix timestamp
-    #[serde(alias = "dt", alias = "ts")]
-    _ts: Option<u64>,
 }
 
 macro_rules! helper {
     ($cb:expr) => {
-        ::axum::routing::get(|q| async move {
-            ::axum::response::Html($cb(q).await.unwrap_or_else(|_| "ERROR".to_owned()))
+        ::axum::routing::get(|s, q| async move {
+            ::axum::response::Html($cb(s, q).await.unwrap_or_else(|_| "ERROR".to_owned()))
         })
     };
 }
@@ -40,6 +41,8 @@ async fn main() -> anyhow::Result<()> {
 
     println!("Server Start: {addr}");
 
+    let client = reqwest::Client::new();
+
     // build our application with a single route
     let app = Router::new()
         .route("/", get(|| async { "Hello, World!" }))
@@ -48,7 +51,8 @@ async fn main() -> anyhow::Result<()> {
         .route("/open/next", helper!(get_open_next))
         .route("/regular", helper!(get_regular_now))
         .route("/regular/now", helper!(get_regular_now))
-        .route("/regular/next", helper!(get_regular_next));
+        .route("/regular/next", helper!(get_regular_next))
+        .with_state(client);
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind(addr).await?;
@@ -58,25 +62,29 @@ async fn main() -> anyhow::Result<()> {
 }
 
 async fn get_open_now(
-    axum::extract::Query(EmbedQuery { t, .. }): axum::extract::Query<EmbedQuery>,
+    State(client): State<reqwest::Client>,
+    Query(EmbedQuery { t, .. }): Query<EmbedQuery>,
 ) -> anyhow::Result<String> {
-    let response = q(Mode::BankaraOpen, Schedule::Now).await?;
+    let response = q(&client, Mode::BankaraOpen, Schedule::Now).await?;
     if response.results.is_empty() {
         anyhow::bail!("")
+    } else {
+        render_embed_html(&response.results[0], t)
     }
-    render_embed_html(&response.results[0], t)
 }
 
 async fn get_open_next(
-    axum::extract::Query(EmbedQuery { t, n, .. }): axum::extract::Query<EmbedQuery>,
+    State(client): State<reqwest::Client>,
+    Query(EmbedQuery { t, n, .. }): Query<EmbedQuery>,
 ) -> anyhow::Result<String> {
-    get_next(Mode::BankaraOpen, n, t).await
+    get_next(&client, Mode::BankaraOpen, n, t).await
 }
 
 async fn get_regular_now(
-    axum::extract::Query(EmbedQuery { t, .. }): axum::extract::Query<EmbedQuery>,
+    State(client): State<reqwest::Client>,
+    Query(EmbedQuery { t, .. }): Query<EmbedQuery>,
 ) -> anyhow::Result<String> {
-    let response = q(Mode::Regular, Schedule::Now).await?;
+    let response = q(&client, Mode::Regular, Schedule::Now).await?;
     if response.results.is_empty() {
         anyhow::bail!("")
     }
@@ -84,12 +92,18 @@ async fn get_regular_now(
 }
 
 async fn get_regular_next(
-    axum::extract::Query(EmbedQuery { t, n, .. }): axum::extract::Query<EmbedQuery>,
+    State(client): State<reqwest::Client>,
+    Query(EmbedQuery { t, n, .. }): Query<EmbedQuery>,
 ) -> anyhow::Result<String> {
-    get_next(Mode::Regular, n, t).await
+    get_next(&client, Mode::Regular, n, t).await
 }
 
-async fn get_next(mode: Mode, n: Option<u8>, t: Option<u32>) -> anyhow::Result<String> {
-    let info = q_after(mode, Schedule::After(n.unwrap_or(1))).await?;
+async fn get_next(
+    client: &reqwest::Client,
+    mode: Mode,
+    n: Option<u8>,
+    t: Option<u32>,
+) -> anyhow::Result<String> {
+    let info = q_after(client, mode, Schedule::After(n.unwrap_or(1))).await?;
     render_embed_html(&info, t)
 }
--- src/splatoon/mod.rs
+++ src/splatoon/mod.rs
@@ -2,26 +2,42 @@ mod mode;
 mod response;
 mod rule;
 mod schedule;
+mod weapon;
 
 pub use mode::*;
 pub use response::*;
 pub use rule::*;
 pub use schedule::*;
 
-pub fn build_url(mode: self::Mode, sche: self::Schedule) -> String {
+macro_rules! get {
+    ($client:expr, $url:expr) => {
+        ::reqwest::Client::get(&$client, &$url)
+            .send()
+            .await?
+            .text()
+            .await?
+    };
+}
+
+fn build_url(mode: self::Mode, sche: self::Schedule) -> String {
     format!("https://spla3.yuu26.com/api/{mode}/{sche}")
 }
 
-pub async fn enquiry(url: String) -> anyhow::Result<self::RawResponse> {
-    let res = reqwest::get(url).await?.text().await?;
+async fn enquiry(client: &reqwest::Client, url: String) -> anyhow::Result<self::RawResponse> {
+    let res = get!(client, url);
     Ok(serde_json::from_str(&res)?)
 }
 
-pub async fn q(mode: self::Mode, sche: self::Schedule) -> anyhow::Result<self::RawResponse> {
-    enquiry(build_url(mode, sche)).await
+pub async fn q(
+    _client: &reqwest::Client,
+    mode: self::Mode,
+    sche: self::Schedule,
+) -> anyhow::Result<self::RawResponse> {
+    enquiry(_client, build_url(mode, sche)).await
 }
 
 pub async fn q_after(
+    _client: &reqwest::Client,
     mode: self::Mode,
     sche: self::Schedule,
 ) -> anyhow::Result<self::RawScheduleInfo> {
@@ -29,7 +45,7 @@ pub async fn q_after(
         anyhow::bail!("q_after requires a Schedule::After variant")
     };
 
-    let response = q(mode, sche).await?;
+    let response = q(_client, mode, sche).await?;
     response
         .results
         .into_iter()
